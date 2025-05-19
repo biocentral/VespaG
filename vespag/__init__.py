9de216bc903ce6a400ba124fcfb58330ef417a8e
Sebastian <sebastian.franz@tum.de>
2025-05-19 14:04:02 +0200

    Changing module exports to deployment setup

---
+++ vespag/__init__.py
@@ -0,0 +1,8 @@
+from .utils import ScoreNormalizer, SAV, compute_mutation_score, mask_non_mutations
+
+__all__ = [
+    "ScoreNormalizer",
+    "SAV",
+    "compute_mutation_score",
+    "mask_non_mutations"
+]
--- vespag/utils/__init__.py
+++ vespag/utils/__init__.py
@@ -3,22 +3,12 @@ from .utils import *
 
 __all__ = [
     "AMINO_ACIDS",
-    "DEFAULT_MODEL_PARAMETERS",
     "GEMME_ALPHABET",
     "SAV",
     "VESPA_ALPHABET",
     "Mutation",
     "ScoreNormalizer",
     "compute_mutation_score",
-    "download",
-    "get_deviceget_embedding_dim",
-    "get_precision",
-    "load_model",
     "mask_non_mutations",
-    "read_gemme_table",
-    "read_mutation_file",
-    "save_async",
-    "setup_logger",
     "transform_scores",
-    "unzip",
 ]
