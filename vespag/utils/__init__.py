3f250ab73f197efabf8a733de1e5a2ac63fb0c8c
Sebastian <sebastian.franz@tum.de>
2025-05-22 16:12:41 +0200

    Adding generate mutations function

--- vespag/utils/__init__.py
+++ vespag/utils/__init__.py
@@ -1,5 +1,6 @@
 from .mutations import *
 from .utils import *
+from .generate_mutations import generate_protein_mutations
 
 __all__ = [
     "AMINO_ACIDS",
@@ -11,4 +12,5 @@ __all__ = [
     "compute_mutation_score",
     "mask_non_mutations",
     "transform_scores",
+    "generate_protein_mutations"
 ]
